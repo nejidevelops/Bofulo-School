82ca49cdca37afa3d1648bc04383b13a494daf4a
Newton Ombese <newtonombese1@gmail.com>
2023-03-23 08:30:45 +0300

    :art:feat: Working on the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder='Username'></input>
+        <input required type="password" placeholder='Password'></input>
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>Don't have an account yet? <Link to='/register'>Register</Link></span>
+      </form>
     </div>
   )
 }

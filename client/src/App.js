82d54d9254e8bbd32d67e2e483b77626a77ba0ac
Newton Ombese <newtonombese1@gmail.com>
2023-03-23 08:30:08 +0300

    :art:feat: Working on routings

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+import './style.scss'
+
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Write from './pages/Write'
+import Home from './pages/Home'
+import Single from './pages/Single'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const Layout = () => {
+  return(
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+])
 
 function App() {
   return (
-    <>
-    </>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
   );
 }
+
 
 export default App;

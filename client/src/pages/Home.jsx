c29447583bbc0ab6562f727ad30018fe68b9caed
Newton Ombese <newtonombese1@gmail.com>
2023-03-23 10:00:37 +0300

    :art:feat: Working on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum",
+      desc: 'Lorem ipsum dolor sit amet',
+      img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum",
+      desc: 'Lorem ipsum dolor sit amet',
+      img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum",
+      desc: 'Lorem ipsum dolor sit amet',
+      img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum",
+      desc: 'Lorem ipsum dolor sit amet',
+      img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ]
   return (
-    <div>
-      
+    <div className='home'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post" key={post.id}>
+              <div className="img">
+                <img src={post.img} alt="" />
+              </div>
+              <div className="content">
+                <Link className='link' to={`/post/${post.id}`}>
+                  <h1>{post.title}</h1>
+                </Link>
+                <p>{post.desc}</p>
+                <button>Read More</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
     </div>
   )
 }
